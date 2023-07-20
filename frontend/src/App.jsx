686b250741daa1c0588bf7cb2d307a26d153a60e
levgen <levgenprog@ya.ru>
2023-07-21 00:31:41 +0600

    show and hide visibility of canvas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
 
+import { useState } from 'react';
 import './App.css';
 import EntityList from './components/EntityList';
 import EntityVisualization from './components/EntityVisualization';
+import Button from './UI/Button/Button';
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleCanvasVisibility = () => {
+    setIsVisible(!isVisible);
+  };
 
   return (
     <div className="App">
       <EntityList></EntityList>
-      <EntityVisualization></EntityVisualization>
+      {!isVisible ? (
+        <Button cls='button edit' change={toggleCanvasVisibility}>Show Enetities Visualization</Button>
+      ) : (
+        <>
+          <EntityVisualization></EntityVisualization>
+          <Button cls='button ' change={toggleCanvasVisibility}>Hide Enetities Visualization</Button>
+        </>
+      )}
+
     </div>
   );
 }
